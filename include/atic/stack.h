4b4edf8bbe9bfaca9b8eb09cd26b478fbbe2d5a9
itamar244 <itamary24@gmail.com>
2018-08-19 13:21:26 +0300

    Merge branch 'master' of https://github.com/itamar244/atic

--- include/atic/stack.h
+++ include/atic/stack.h
@@ -6,38 +6,6 @@
 
 namespace atic {
 
-template<typename Node>
-std::pair<Node*, Node*> CopyNode(const Node* node) {
-  if (node == nullptr) return {nullptr, nullptr};
-  Node* copy = new Node(node->value);
-  Node* cur = copy;
-
-  node = node->next;
-  while (node != nullptr) {
-    cur = cur->next = new Node(node->value);
-    node = node->next;
-  }
-
-  return {copy, cur};
-}
-
-template<typename Node>
-inline void DeleteNode(Node* node) {
-  while (node) {
-    auto next = node->next;
-    delete node;
-    node = next;
-  }
-}
-
-template<typename T>
-struct Node {
-  T value;
-  Node* next;
-
-  Node(T value, Node* next = nullptr) : value(value), next(next) {}
-};
-
 template<typename T>
 class Stack {
 public:
